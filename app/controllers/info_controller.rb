e838a32c8d4d2a108fedd389b03c34d026fb72cb
Edgar Aguilar <edgar.aguilar@michelada.io>
2022-06-08 15:47:17 -0500

    create info controller and add base endpoint

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class InfoController < ApplicationController
+  def build_info
+    @dummy_info = {
+      build_id: 'test 1',
+      build_date: 'test 2',
+      build_branch: 'test 3'
+    }
+
+    render json: @dummy_info
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/build_info', to: 'info#build_info'
 end
