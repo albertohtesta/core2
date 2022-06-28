0fda5e9aa0b7c4b907ad2c5c72a251964a469220
Edgar Aguilar <edgar.aguilar@michelada.io>
2022-06-28 13:19:26 -0500

    feat: add test for update endpoint in password controller

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -16,4 +16,18 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal(response_body, expected_response.as_json)
   end
+
+  test "should update password for current user" do
+    expected_response = {
+      message: "Password set successfully. A confirmation mail was sent."
+    }
+
+    patch api_v1_passwords_path,
+          params: { credentials: { password: "Somepassword123!", new_password: "Newpassword123!" } },
+          headers: { "Authorization" => "1s34s6t8g" }
+
+    response_body = JSON.parse(response.body)
+    assert_response :success
+    assert_equal(response_body, expected_response.as_json)
+  end
 end
--- test/services/password_service_test.rb
+++ test/services/password_service_test.rb
@@ -2,32 +2,32 @@
 
 require "test_helper"
 
-class RegistrationServiceTest < ActiveSupport::TestCase
+class PasswordServiceTest < ActiveSupport::TestCase
   test "should update password" do
-    @password_service = PasswordService.new({password: "Somepassword123!", new_password: "Newpassword123!", access_token: "1z34s6t8g"})
-   
+    @password_service = PasswordService.new({ password: "Somepassword123!", new_password: "Newpassword123!",
+                                              access_token: "1z34s6t8g" })
+
     assert(@password_service.update_password)
   end
 
   test "should not update password without password" do
-    @password_service = PasswordService.new({new_password: "Newpassword123!", access_token: "1z34s6t8g"})
+    @password_service = PasswordService.new({ new_password: "Newpassword123!", access_token: "1z34s6t8g" })
     @password_service.update_password
-    
+
     assert_equal("missing required parameter params[:previous_password]", @password_service.error.message)
   end
 
   test "should not update password without new_password" do
-    @password_service = PasswordService.new({password: "Somepassword123!", access_token: "1z34s6t8g"})
+    @password_service = PasswordService.new({ password: "Somepassword123!", access_token: "1z34s6t8g" })
     @password_service.update_password
-    
+
     assert_equal("missing required parameter params[:proposed_password]", @password_service.error.message)
   end
 
   test "should not update password without access_token" do
-    @password_service = PasswordService.new({password: "Somepassword123!", new_password: "Newpassword123!"})
+    @password_service = PasswordService.new({ password: "Somepassword123!", new_password: "Newpassword123!" })
     @password_service.update_password
-    
+
     assert_equal("missing required parameter params[:access_token]", @password_service.error.message)
   end
-
 end
