2eba7e9942d0f426eea07eb207959e078267999f
Cesar Gomez <cesargomez@89@gmail.com>
2022-06-29 14:50:55 -0500

    fix env vars

--- app/services/cognito_service.rb
+++ app/services/cognito_service.rb
@@ -4,13 +4,13 @@
 class CognitoService < ApplicationService
   CLIENT = Aws::CognitoIdentityProvider::Client.new(
     region: ENV.fetch("AWS_REGION", nil),
-    access_key_id: ENV.fetch("ACCESS_KEY_ID", nil),
-    secret_access_key: ENV.fetch("SECRET_ACCESS_KEY", nil),
+    access_key_id: ENV.fetch("COGNITO_ADMIN_ACCESS_KEY", nil),
+    secret_access_key: ENV.fetch("COGNITO_ADMIN_SECRET_KEY", nil),
     stub_responses: Rails.env.test?
   ).freeze
 
-  CLIENT_ID = ENV.fetch("AWS_COGNITO_CLIENT_ID", nil).freeze
-  POOL_ID = ENV.fetch("AWS_COGNITO_POOL_ID", nil).freeze
+  CLIENT_ID = ENV.fetch("AWS_COGNITO_USER_POOL_CLIENT_ID", nil).freeze
+  POOL_ID = ENV.fetch("AWS_COGNITO_USER_POOL", nil).freeze
 
   def initialize(user_object)
     @user_object = user_object
