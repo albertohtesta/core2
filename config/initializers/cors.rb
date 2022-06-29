bcdd90c66dde51cf853d1197b53b85f0cb683258
Cesar Gomez <cesargomez@89@gmail.com>
2022-06-29 10:33:05 -0500

    update environments

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:4000", '"qa-core-api.nordhen.com"'
+    origins "localhost:4000", '"https://qa-core.nordhen.com"'
     resource "*", headers: :any, methods: %i[get post patch put]
   end
 end
