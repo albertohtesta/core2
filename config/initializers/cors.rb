3be4640b5fc59f6c27e688e93257b1868d6512dd
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-07-01 11:33:45 -0500

    fix: add delete method to cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:4000", "https://qa-core.nordhen.com"
-    resource "*", headers: :any, methods: %i[get post patch put]
+    resource "*", headers: :any, methods: %i[get post patch put delete]
   end
 end
