9d2dd4dde058ab12291e656860b97fc4bc3d0e3b
Cesar Gomez <cesargomez@89@gmail.com>
2022-07-04 22:26:38 -0500

    fix: rubocop

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ if [ -f /core-backend/tmp/pids/server.pid ]; then
   rm /core-backend/tmp/pids/server.pid
 fi
 
-bundle check --path vendor/bundle || bundle install --path vendor/bundle --jobs `expr $(cat /proc/cpuinfo | grep -c "cpu cores") - 1` --retry 3
+bundle check --path /core-backend/vendor/bundle || bundle install --path vendor/bundle --jobs `expr $(cat /proc/cpuinfo | grep -c "cpu cores") - 1` --retry 3
 
 exec "$@"
