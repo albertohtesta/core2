c97d0737dbb9ee571aa6da3a528c10597d365735
Karla Elizabeth Alvarez Castillo <karla.alvarez@michelada.io>
2022-07-07 11:06:31 -0500

    feat: add column enabled to user model

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_204005) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_200528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_204005) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_enabled", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
