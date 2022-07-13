72cd6a854117a4e6f93cd7e1855c624d256e92ab
Karla Elizabeth Alvarez Castillo <karla.alvarez@michelada.io>
2022-07-13 15:54:05 -0500

    chore: Update rails to 7.0.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_230526) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_204005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
