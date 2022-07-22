d86c0e50a842b31dacadd1879795a7b395d0d373
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-07-22 11:29:24 -0500

    fix: fix schema file

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_170800) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_204005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,4 +25,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_170800) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
+
 end
