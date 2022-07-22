ba53dd41906eafcac2b4300f025076f4c1d35ff5
Karla Álvarez <karla.alvarez@michelada.io>
2022-07-22 20:29:04 +0000

    fix: return a 200 status when the user doesn't exist

--- app/controllers/api/v1/users/forgot_passwords_controller.rb
+++ app/controllers/api/v1/users/forgot_passwords_controller.rb
@@ -10,7 +10,7 @@ module Api
         def create
           @password_service = PasswordService.new(recover_password_params)
 
-          if @password_service.recover_password
+          if @password_service.recover_password || (@password_service.error.class == Aws::CognitoIdentityProvider::Errors::UserNotFoundException)
             render json: { message: "Password recovery email sent" }, status: :ok
           else
             render json: { message: @password_service.error }, status: :bad_request
