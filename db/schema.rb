b8bf34fc562240d75196cc7613d200c8afc3f1ec
David Jarillo <david.jarillo@michelada.io>
2022-08-25 10:46:03 -0700

    feat: running migrations of renaming uid to uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_221818) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_215237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,13 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_221818) do
     t.string "name"
     t.string "email", null: false
     t.string "status"
-    t.string "uid"
+    t.string "uuid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_enabled", default: true
     t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["uid"], name: "index_users_on_uid", unique: true
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
 end
