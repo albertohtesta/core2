92e085ed0834b05192ed748c312abb9088d8391e
David Jarillo <david.jarillo@michelada.io>
2022-09-12 09:59:33 -0700

    feat: adding user admin into seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+user = UserRepository.find_by_email("admin@nordhen.com")
+unless user
+  User.create(name: "Norden Administrator", email: "admin@nordhen.com", roles: ["admin"])
+  puts "User admin created!!"
+end
