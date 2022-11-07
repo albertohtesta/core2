d487466d79dceb408ea7d0a55e5b9029111652eb
David Jarillo <david.jarillo@michelada.io>
2022-11-07 12:41:08 -0800

    feat: removing older tests

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -8,19 +8,6 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
     login_as(user)
   end
 
-  test "should update a password for new user" do
-    expected_response = "Password updated"
-
-    post api_v1_passwords_path,
-         params: { credentials: { username: "test-registration@test.com", password: "otroP4$ssword",
-                                  new_password: "Password321¡" } },
-         as: :json
-
-    response_body = JSON.parse(response.body)
-    assert_response :success
-    assert_equal(response_body["message"], expected_response)
-  end
-
   test "should update password for current user" do
     expected_response = "Password set successfully. A confirmation mail was sent."
 
